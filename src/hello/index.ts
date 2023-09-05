1cc5c5a21a6d59e66afcad387e2cfed262b9dffd
Nath Paiva <hello@nathpaiva.com.br>
2023-09-05 15:53:15 -0700

    feat: adds db access

--- src/hello/index.ts
+++ src/hello/index.ts
@@ -1,30 +1,23 @@
-// import {
-//   DynamoDBClient,
-//   GetItemCommand,
-//   GetItemCommandInput,
-// } from '@aws-sdk/client-dynamodb'
-// import { marshall, unmarshall } from '@aws-sdk/util-dynamodb'
+import {
+  DynamoDBClient,
+  GetItemCommand,
+  GetItemCommandInput,
+} from '@aws-sdk/client-dynamodb'
+import { marshall } from '@aws-sdk/util-dynamodb'
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 
-// const client = new DynamoDBClient({ region: 'us-west-2' })
+const client = new DynamoDBClient({
+  region: process.env.REGION,
+  endpoint: process.env.DB_ENDPOINT,
+})
 
-// const params: GetItemCommandInput = {
-//   TableName: 'forum_table',
-//   // Convert the key JavaScript object you are retrieving to the required DynamoDB format. The format of values specifies
-//   // the datatype. The following list demonstrates different datatype formatting requirements:
-//   // HashKey: "hashKey",
-//   // NumAttribute: 1,
-//   // BoolAttribute: true,
-//   // ListAttribute: [1, "two", false],
-//   // MapAttribute: { foo: "bar" },
-//   // NullAttribute: null
-//   Key: marshall({
-//     status: 'PENDING',
-//     id_topic: 'Topic#01',
-//   }),
-// }
-
-var a = ''
+const params: GetItemCommandInput = {
+  TableName: 'forumTable',
+  Key: marshall({
+    status: 'PENDING',
+    id_topic: 'Topic#01',
+  }),
+}
 
 export const hello = async (
   event: APIGatewayProxyEvent,
@@ -34,8 +27,9 @@ export const hello = async (
       throw new Error('You must provide an id')
     }
 
-    // const results = await client.send(new GetItemCommand(params))
-    // console.log(results)
+    const results = await client.send(new GetItemCommand(params))
+
+    console.log(results)
 
     return {
       statusCode: 200,
